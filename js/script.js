c9dc5c8107992345e29ccd7a103f2bb895509ebb
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-09 18:03:03 +0300

    add krik

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 let x = document.querySelector('#myAudio');
 let krik = false;
-let kricInterval = 10000;
+let krikInterval = 10000;
 let interval = 0;
 
-document.querySelector('.halloween').onclick = randonInterval;
+document.querySelector('.halloween').onclick = randomInterval;
 
 function playAudio() {
+    if (krik) {
+        x.play();
+        innerval = 0;
+    } else {
+        x.pause();
+    }
+};
 
-}
-
-function randonInterval() {
+function randomInterval() {
     if (interval == 0 && krik == false) {
-        interval = Math.random() * kricInterval;
-        console.log(interval);
+        interval = Math.random() * krikInterval;
+        // console.log(interval);
         setTimeout(playAudio, interval);
-        document.querySelector('.halloween').classList.add('.halloween-active')
+        document.querySelector('.halloween').classList.add('halloween-active');
+        krik = true;
     };
 };
